c71796e232a120c9a42b8be40c4d952d29756542
chienpn <chienngoc50@gmail.com>
2021-05-21 19:38:12 +0700

    third update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent implements OnInit {
+  ImagePath1: string;
+  ImagePath2: string;
+  ImagePath3: string;
+  ImagePath4: string;
+  items = Array.from({length: 10}).map((_, i) => `Tên sách review ${i}`);
+  constructor() {
+    this.ImagePath1 = 'assets/images/anh1.jpg';
+    this.ImagePath2 = 'assets/images/anh2.jpg';
+    this.ImagePath3 = 'assets/images/anh3.jpg';
+    this.ImagePath4 = 'assets/images/anh4.jpg';
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
   }
